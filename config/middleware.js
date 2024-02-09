1e19c8c0328cce81258d505b6d3203dd96833971
Ranik <er.rahul0314@gmail.com>
2024-02-09 15:12:16 +0530

    created home and view page

---
+++ config/middleware.js
@@ -0,0 +1,12 @@
+console.log("flash middleware loaded");
+
+//This middleware is responsible for setting flash messages in the response locals object,
+// making them available to the views.
+module.exports.setFlash = function(req, res, next){
+    res.locals.flash = {
+        "success": req.flash("success"),
+        "error": req.flash("error")
+    }
+
+    next();
+}
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,20 @@ const express = require('express');
 //creates an instance of the Express router
 const router = express.Router();
 
+const multer = require('multer');
+const upload = multer({ dest:'uploadFiles' })
+
 //The require() function is used to import the router modules defined in separate files
-//const homeController = require('../controllers/home_controller');
+const homeController = require('../controllers/home_controller');
+const viewController = require('../controllers/view_controller')
 
 console.log('router loaded')
 
 //When a GET request is made to '/', it invokes the home function from the homeController
-//router.get('/', homeController.home);
+router.get('/', homeController.home);
+router.get('/delete/:id', homeController.delete);
+router.get('/view/:id', viewController.view);
+
+router.post('/upload',upload.single('file'), homeController.upload);
 
 module.exports = router;
